b257d70cbb05af46424f1183a593372a782801c1
Florent1711 <graphit01@hotmail.com>
2024-04-23 11:58:21 +0200

    Ajout de 3-to_str.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,5 +3,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """A type-annotated funct concat takes strings str1 & str2 as arguments and returns a concatenated string"""
+    """concat strings str1 & str2"""
     return str1 + str2
