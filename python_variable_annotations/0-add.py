3a50f085534fc020af46186fc91d0e3f36800e24
Florent1711 <graphit01@hotmail.com>
2024-04-23 10:31:01 +0200

    test Jetbrain IDE

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Fonction add annotée par type qui prend un  float a et b"""
+"""Fonction add annotée par type qui prend un float en a et b"""
 
-def add (x: float, y: float) -> float:
+
+def add(x: float, y: float) -> float:
     return x + y
