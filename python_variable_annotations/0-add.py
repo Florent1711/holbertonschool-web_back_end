f7f32eb548eb781ac8de6d98026567a3d7b2fb1d
Florent1711 <graphit01@hotmail.com>
2024-04-23 10:44:03 +0200

    test Jetbrain IDE

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
-
-    a = 1
-    b = 2
+def add(x: float, y: float) -> float:
     print("{} + {} = {}".format(a, b, add(a, b)))
