d16d85f7d404227c08ea7f69216bdb12482c89e0
Florent1711 <graphit01@hotmail.com>
2024-04-23 11:18:02 +0200

    test Jetbrain IDE

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A type-annotated funct concat takes strings str1 & str2 as arguments and returns a concatenated string"""
+    return str1 + str2
