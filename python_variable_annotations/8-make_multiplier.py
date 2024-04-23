5f48abea84a600c7b5809cc72d516d8910b64ac8
Florent1711 <graphit01@hotmail.com>
2024-04-23 14:50:46 +0200

    modification de 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,9 +2,12 @@
 """make_multiplier that takes a float multiplier"""
 
 
+from typing import Callable
+
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    float multiplier as argument and returns multiplies a float by multiplier.
+    Take float multiplier as a argment returns multiplies a float by multiplier
     """
     def multiplier_function(x: float) -> float:
         return x * multiplier
