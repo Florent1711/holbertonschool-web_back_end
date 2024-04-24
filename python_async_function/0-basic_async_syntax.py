41884d1cbd7f479ff26d01b120fde1704ad5fe80
Florent1711 <graphit01@hotmail.com>
2024-04-24 12:18:30 +0200

    Ajout de README.md et 0-basic_async.py

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine that takes in an integer argument"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Waits for a random delay betweeen 0 and max_delay (inclusive) seconds.
+    Returns the delay as a float
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+# Example usage:
+async def main():
+    random_delay = await wait_random()
+    print(f"Random delay: {random_delay:.2f} seconds")
+    
+if __name__ == "__main__":
+    asyncio.run(main())
