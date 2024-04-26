948ea49290b3009829b10d2fd2c1f5f146af9315
Florent1711 <graphit01@hotmail.com>
2024-04-26 16:13:25 +0200

    Ajout de 2-measure_runtime.py

--- python_variable_annotations/3-to_str.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""A type-annotated to_str """
-
-
-def to_str(n: float) -> str:
-    """to_str that takes a float n as argument and returns the string"""
-    return format(n, )
