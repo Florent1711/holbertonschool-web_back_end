2a42c78e765ed3b9ad17cca2509b627dc84d7893
Florent1711 <graphit01@hotmail.com>
2024-04-30 14:12:35 +0200

    Modif de 10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,16 +3,9 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """Update the topics of a school document based on the school name.
+    """Update the topics of a school document based on the school name."""
 
-    Parameters:
-    mongo_collection: pymongo collection object
-    name (str): the school name to update
-    topics (list of str): the list of topics approached in the school
-    """
-
-    # Update the document
-    updated_document = mongo_collection.update_many(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
