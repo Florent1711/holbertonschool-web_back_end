4eeac4a87411fe82d715b518de4b08697b717696
Florent1711 <graphit01@hotmail.com>
2024-05-07 14:13:40 +0200

    ajout de 2-arrow.js

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighbourhoodsList() {
+    this.sanFranciscoNeighbourhoods = ['SOMA', 'Union Square'];
+
+    const self = this;
+    this.addNeighborhood = (newNeighbourhood) => {
+        self.sanFranciscoNeighbourhoods.push(newNeighbourhood);
+        return self.sanFranciscoNeighbourhoods;
+    };
+}
