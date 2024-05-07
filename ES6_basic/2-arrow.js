d401a6a1df8dac3b278ef74f06aec559981f126a
Florent1711 <graphit01@hotmail.com>
2024-05-07 14:50:42 +0200

    Correction eslint sur les fichiers 0-constants, 1-block-scoped.js et 2-arrow.js

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default function getNeighbourhoodsList() {
-    this.sanFranciscoNeighbourhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighbourhoods = ['SOMA', 'Union Square'];
 
-    const self = this;
-    this.addNeighborhood = (newNeighbourhood) => {
-        self.sanFranciscoNeighbourhoods.push(newNeighbourhood);
-        return self.sanFranciscoNeighbourhoods;
-    };
+  const self = this;
+  this.addNeighborhood = (newNeighbourhood) => {
+    self.sanFranciscoNeighbourhoods.push(newNeighbourhood);
+    return self.sanFranciscoNeighbourhoods;
+  };
 }
