561b3042afde5c150d4620403395349309ffc7a2
Florent1711 <graphit01@hotmail.com>
2024-05-07 12:08:47 +0200

    Ajout de README.md et 0-constants.js

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,11 @@
+function taskFirst() {
+    // Assuming you have variables that can be declared with const
+    const someVariaable = 'value';
+
+}
+
+function taskNext() {
+    // Assuming you have variables that can be declared with let
+    let anotherVariaable = 'another value';
+    // ... rest of your code
+}
